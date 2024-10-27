faa7290566a8f6c5ded38d84311f6d8bbcb94deb
devsamahd <samahd626@gmail.com>
2024-10-28 00:33:48 +0100

    ...

--- web3/utils/url.ts
+++ web3/utils/url.ts
@@ -2,19 +2,27 @@ import { env } from "@/configs";
 
 export function getImageUrl(url: string, width?: number, height?: number) {
   if (!url) return "";
+  
+  // Extract the filename from the URL
+  const fileName = url.split('/').pop(); // Get the last segment of the URL
   const protocol = url.split(":")[0];
   const q = width && height ? `?w=${width}&h=${height}` : "";
+  
   if (protocol === "http") {
     return url + q;
   }
+  
   try {
-    return env.cdnBaseUrl + url + q;
+    return env.cdnBaseUrl + fileName + q; // Use the extracted filename
   } catch (err) {
-    return url + q;
+    return url + q; // Fallback to the original URL
   }
 }
 
 export function getAvatarUrl(url: string) {
   if (!url) return "/images/default-avatar.png";
-  return env.cdnBaseUrl + url;
+
+  const fileName = url.split('/').pop(); 
+
+  return `${env.cdnBaseUrl}avatars/${fileName}`;
 }
