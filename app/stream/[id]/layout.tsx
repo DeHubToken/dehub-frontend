ca68e97116dbd63ccb9d4e3afa51935824e68ea2
Yash Ramoliya <yashramoliya12@gmail.com>
2024-11-08 20:47:54 +0530

    fix(video): video icons are not loading

--- app/stream/[id]/layout.tsx
+++ app/stream/[id]/layout.tsx
@@ -1,12 +1,8 @@
-import { safeParseCookie } from "@/libs/cookies";
-import { getNFT } from "@/services/nfts";
-import { getImageUrl } from "@/web3/utils/url";
 import { Metadata } from "next";
-import { cookies } from "next/headers";
 
 type Props = {
   children: React.ReactNode;
-  params: { id: string};
+  params: { id: string };
 };
 
 // Function to dynamically generate metadata based on NFT data
