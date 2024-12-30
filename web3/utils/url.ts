081aeef6e5ca338e18e26ca9f102b19c53dd8494
Attila Csanyi <attila.csnyi@yahoo.co.uk>
2024-12-30 16:12:27 +0100

    feat: use dehub specific default banner on user profile (fix #11) (#12)
    * chore: update default banner image and use Dehub default one
    
    * chore: replace default banner image with a new PNG version
    
    * refactor: update default cover image path

--- web3/utils/url.ts
+++ web3/utils/url.ts
@@ -2,18 +2,18 @@ import { env } from "@/configs";
 
 export function getImageUrl(url: string, width?: number, height?: number) {
   if (!url) return "";
-  
+
   // Extract the filename from the URL
-  const fileName = url.split('/').pop(); // Get the last segment of the URL
+  const fileName = url.split("/").pop(); // Get the last segment of the URL
   const protocol = url.split(":")[0];
   const q = width && height ? `?w=${width}&h=${height}` : "";
-  
+
   if (protocol === "http") {
     return url + q;
   }
-  
+
   try {
-    return env.cdnBaseUrl +"images/" + fileName + q; // Use the extracted filename
+    return env.cdnBaseUrl + "images/" + fileName + q; // Use the extracted filename
   } catch (err) {
     return url + q; // Fallback to the original URL
   }
@@ -22,15 +22,15 @@ export function getImageUrl(url: string, width?: number, height?: number) {
 export function getAvatarUrl(url: string) {
   if (!url) return "/images/default-avatar.png";
 
-  const fileName = url.split('/').pop(); 
+  const fileName = url.split("/").pop();
 
   return `${env.cdnBaseUrl}avatars/${fileName}`;
 }
 
 export function getCoverUrl(url: string) {
-  if (!url) return "/images/default-avatar.png";
+  if (!url) return "/images/default-banner.png";
 
-  const fileName = url.split('/').pop(); 
+  const fileName = url.split("/").pop();
 
   return `${env.cdnBaseUrl}covers/${fileName}`;
 }
