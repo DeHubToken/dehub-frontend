f8e7f33d05582e3cabb0071dba97e25f95489a37
Ajay Panchal <ajay.immanent@gmail.com>
2025-01-22 14:28:39 +0530

    replacing  upload-feed directory with upload directory  to make sure single route for upload.

--- app/upload/page.tsx
+++ app/upload/page.tsx
@@ -1,13 +1,16 @@
 import { getCategories } from "@/services/categories";
 
-import { UploadForm } from "./components/upload-form";
+import { UploadForm } from "./components/upload-form"; 
 
 export default async function Page() {
   const categoriesRes = await getCategories();
+
+  
   let categories: string[] = [];
 
   if (categoriesRes.success) {
     categories = categoriesRes.data;
-  } 
+  }
+
   return <UploadForm categories={categories} />;
 }
