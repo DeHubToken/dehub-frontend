3ab970efdf5240da30ccf73a752e840bd96002d8
Ajay Panchal <ajay.immanent@gmail.com>
2025-01-31 11:53:27 +0530

    updating username route

---
+++ app/(user)/[username]/components/profile-action.tsx
@@ -0,0 +1,79 @@
+"use client";
+
+import type { User } from "@/stores";
+
+import { CirclePlus, UserPlus } from "lucide-react";
+import { toast } from "sonner";
+
+import { TipModal } from "@/app/stream/[id]/components/tip-modal";
+
+import { Button } from "@/components/ui/button";
+
+import { useActiveWeb3React } from "@/hooks/web3-connect";
+
+import { useFollow } from "../hooks/use-follow";
+import { useUnFollow } from "../hooks/use-unfollow";
+import { SubscriptionModal } from "./subscription-modal";
+import { getPlans } from "@/services/subscription-plans";
+import { useEffect, useState } from "react";
+
+type Props = {
+  user: User;
+  username: string;
+  aboutMe?:string
+};
+
+type FollowButtonProps = { user: User };
+
+function FollowButton(props: FollowButtonProps) {
+  const { user } = props;
+  const { follow } = useFollow({ user });
+  return (
+    <Button variant="gradientOne" size="sratch" className="gap-2 py-5" onClick={follow}>
+      <UserPlus className="size-5" /> Follow
+    </Button>
+  );
+}
+
+type UnfollowButtonProps = { user: User };
+
+function UnfollowButton(props: UnfollowButtonProps) {
+  const { user } = props;
+  const { unfollow } = useUnFollow({ user });
+  return (
+    <Button variant="gradientOne" size="sratch" className="gap-2 py-5" onClick={unfollow}>
+      <UserPlus className="size-5" /> Unfollow
+    </Button>
+  );
+}
+
+export async function ProfileAction(props: Props) {
+  const { user } = props;
+  const { account } = useActiveWeb3React();
+  const isFollowing = user.followers?.includes(account?.toLowerCase() || "");
+  const [plans,setPlans] = useState([])
+  const fetchMyPlans = async () => {
+    if(user?.address){
+    const { success, error, data }: any = await getPlans({ address: (user?.address).toLowerCase() });
+    if (!success && !data?.plans) {
+      toast.error(error);
+      return;
+    }
+    setPlans(data.plans)
+  }
+  };
+
+  useEffect(() => {
+    fetchMyPlans()
+  }, [])
+
+  return (
+    <div className="flex w-full max-w-screen-xs flex-wrap items-start justify-start gap-3 overflow-hidden sm:gap-4">
+      <SubscriptionModal plans={plans} displayName={user.displayName} aboutMe={user?.aboutMe} avatarImageUrl={user.avatarImageUrl}/>
+      <TipModal tokenId={0} to={user.address!} />
+
+      {isFollowing && <UnfollowButton user={user} />}
+      {!isFollowing && <FollowButton user={user} />}
+    </div>
+  );
+}
---
+++ app/(user)/[username]/components/user-info.tsx
@@ -0,0 +1,49 @@
+import "server-only";
+
+import type { User } from "@/stores";
+
+import { Badge } from "@/components/ui/badge";
+
+import { formatToUsDate } from "@/libs/date-time";
+
+type Props = {
+  user: User;
+};
+
+export function UserFollowingInfo(props: Props) {
+  const { user } = props;
+  return (
+    <div className="flex size-auto max-w-screen-xs flex-wrap items-start justify-start gap-3 sm:gap-4">
+      <Badge variant="secondary" className="px-4 text-[11px]">
+        <span>Followers</span> <span>{user.followers?.length || 0}</span>
+      </Badge>
+      <Badge variant="secondary" className="px-4 text-[11px]">
+        <span>Following</span> <span>{user.followings?.length || 0}</span>
+      </Badge>
+      <Badge variant="secondary" className="px-4 text-[11px]">
+        <span>Likes</span> <span>{user.likes?.length || 0}</span>
+      </Badge>
+      <Badge variant="secondary" className="px-4 text-[11px]">
+        <span>Tips earned</span> <span>{user.receivedTips || 0}</span>
+      </Badge>
+      <Badge variant="secondary" className="px-4 text-[11px]">
+        <span>Tips given</span> <span>{user.sentTips || 0}</span>
+      </Badge>
+    </div>
+  );
+}
+
+export function UserDescription(props: Props) {
+  const { user } = props;
+  return (
+    <div className="size-auto space-y-4">
+      <p className="max-w-screen-xs text-sm">{user.aboutMe || "No description"}</p>
+
+      {user.createdAt ? (
+        <p className="text-sm">Joined at {formatToUsDate(user.createdAt)}</p>
+      ) : (
+        "No joined yet"
+      )}
+    </div>
+  );
+}
