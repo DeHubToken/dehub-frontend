f2034da80c16525805df3a64455922684f578810
Yash Ramoliya <yashramoliya12@gmail.com>
2025-02-05 21:30:24 +0530

    feat: add sidebar component for new design system
    Add new sidebar component, keep old one for fallback.
    Update sidebar icons. Optimize sidebar icons and reduce it's size.

--- components/icons/leaderboard.tsx
+++ components/icons/leaderboard.tsx
@@ -3,25 +3,11 @@ import type { SVGProps } from "react";
 import * as React from "react";
 
 const LeaderBoard = (props: SVGProps<SVGSVGElement>) => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    width={17}
-    height={17}
-    viewBox="0 0 24 24"
-    fill="none"
-    stroke="currentColor"
-    strokeWidth="2"
-    strokeLinecap="round"
-    strokeLinejoin="round"
-    className="lucide lucide-trophy"
-    {...props}
-  >
-    <path d="M6 9H4.5a2.5 2.5 0 0 1 0-5H6" />
-    <path d="M18 9h1.5a2.5 2.5 0 0 0 0-5H18" />
-    <path d="M4 22h16" />
-    <path d="M10 14.66V17c0 .55-.47.98-.97 1.21C7.85 18.75 7 20.24 7 22" />
-    <path d="M14 14.66V17c0 .55.47.98.97 1.21C16.15 18.75 17 20.24 17 22" />
-    <path d="M18 2H6v7a6 6 0 0 0 12 0V2Z" />
+  <svg viewBox="0 0 20 19" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+    <path
+      d="M1.188 18.375a.9.9 0 0 1-.663-.275.9.9 0 0 1-.275-.663V7.813q0-.387.275-.662a.9.9 0 0 1 .663-.275H4.5q.388 0 .662.275a.9.9 0 0 1 .276.662v9.625q0 .389-.276.663a.9.9 0 0 1-.662.275zm7.15 0a.9.9 0 0 1-.663-.275.9.9 0 0 1-.275-.663V1.813q0-.387.275-.662a.9.9 0 0 1 .663-.275h3.325q.387 0 .662.275a.9.9 0 0 1 .275.663v15.625q0 .387-.275.662a.9.9 0 0 1-.662.275zm7.162 0a.9.9 0 0 1-.662-.275.9.9 0 0 1-.275-.663V9.813q0-.387.274-.662a.9.9 0 0 1 .663-.275h3.313q.387 0 .662.275a.9.9 0 0 1 .275.662v7.626q0 .387-.275.662a.9.9 0 0 1-.663.275z"
+      fill="currentColor"
+    />
   </svg>
 );
 export default LeaderBoard;
