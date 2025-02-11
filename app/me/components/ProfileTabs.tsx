960da77c8c5777e2b6fb85c2f3c41dd3186c557e
Ajay Panchal <ajay.immanent@gmail.com>
2025-02-11 15:37:47 +0530

    add some css.

--- app/me/components/ProfileTabs.tsx
+++ app/me/components/ProfileTabs.tsx
@@ -1,6 +1,8 @@
 import React, { useEffect } from "react";
 import Link from "next/link";
 
+import { Categories } from "@/app/components/categories";
+
 import ProfileTabViewServer from "./TabView";
 
 const tabs = [
@@ -14,33 +16,47 @@ export default function ProfileTabView({
   searchParams,
   isOwner
 }: {
-  user: any; 
+  user: any;
   searchParams: any;
   isOwner: boolean;
 }) {
-  const activeTab=searchParams?.tab??"video"
+  const activeTab = searchParams?.tab ?? "video";
   return (
     <div>
-      <div className="flex w-full">
-        {tabs.map(({ key, label }) => (
-          <Link
-            key={key}
-            href={`/${isOwner?"me":user?.username??user?.address??""}?tab=${key}`}
-            className={`rounded-t-lg border-b-2 px-6 py-2 ${
-              activeTab === key
-                ? "border-blue-500 text-blue-500"
-                : "border-transparent text-gray-500"
-            }`}
-          >
-            {label}
-          </Link>
-        ))}
+      <div className="flex flex-wrap justify-between align-middle">
+        <div className="flex flex-wrap">
+          {tabs.map(({ key, label }) => (
+            <Link
+              key={key}
+              href={`/${isOwner ? "me" : (user?.username ?? user?.address ?? "")}?tab=${key}`}
+              className={`rounded-t-lg border-b-2 px-6 py-2 ${
+                activeTab === key
+                  ? "border-blue-500 text-blue-500"
+                  : "border-transparent text-gray-500"
+              }`}
+            >
+              {label}
+            </Link>
+          ))}
+        </div>
+
+        <div className=" max-w-[50vh]">
+          <Categories
+            title={"type.toUpperCase()"}
+            category={"category"}
+            range={"range"}
+            type={"type"}
+            q={"q"}
+            sortBy={"sortBy"}
+          />
+        </div>
       </div>
+
       <ProfileTabViewServer
         user={user}
         isOwner={isOwner}
         activeTab={activeTab}
-        searchParams={searchParams} 
+        searchParams={searchParams}
       />
     </div>
   );
