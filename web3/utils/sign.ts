eff4c37906dec4eac3c6d6ee8b997834e769dc7a
Alhaji <alhaji4megahit@gmail.com>
2025-05-25 00:46:33 +0100

    fix: signature popup and pnpm lock

--- web3/utils/sign.ts
+++ web3/utils/sign.ts
@@ -4,21 +4,26 @@ type Library = JsonRpcSigner;
 
 let isSigning = false;
 
-export const performPersonalSign = (library: Library, account: string, signMessage: string) => {
+export const performPersonalSign = async (
+  library: Library,
+  account: string,
+  signMessage: string
+) => {
   if (!account) throw new Error("Account invalid.");
   if (!library) throw new Error("Please connect to your wallet.");
   if (typeof library.signMessage !== "function") {
-    throw new Error("Provider not ready: `signMessage` is not available.");
+    throw new Error("Provider not ready: signMessage is not available.");
   }
+
   if (isSigning) {
     throw new Error("Signature request already in progress.");
   }
 
   isSigning = true;
   try {
-    return library.signMessage(signMessage);
+    const signature = await library.signMessage(signMessage);
+    return signature;
   } finally {
     isSigning = false;
   }
-  // return library.signMessage(signMessage);
 };
