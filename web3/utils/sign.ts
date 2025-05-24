66aba7a00a0db502b7b8379403c57c296f573dff
Alhaji <alhaji4megahit@gmail.com>
2025-05-25 00:39:31 +0100

    fix: fixing double popups

--- web3/utils/sign.ts
+++ web3/utils/sign.ts
@@ -2,17 +2,23 @@ import type { JsonRpcSigner } from "@ethersproject/providers";
 
 type Library = JsonRpcSigner;
 
-// export const performPersonalSign = (library: Library, account: string, signMessage: string) => {
-//   if (!account) throw new Error("Account invalid.");
-//   if (!library) throw new Error("Please connect to your wallet.");
-//   if (!library.signMessage) throw new Error("Provider not ready");
-//   return library.signMessage(signMessage);
-// };
+let isSigning = false;
+
 export const performPersonalSign = (library: Library, account: string, signMessage: string) => {
   if (!account) throw new Error("Account invalid.");
   if (!library) throw new Error("Please connect to your wallet.");
   if (typeof library.signMessage !== "function") {
     throw new Error("Provider not ready: `signMessage` is not available.");
   }
-  return library.signMessage(signMessage);
+  if (isSigning) {
+    throw new Error("Signature request already in progress.");
+  }
+
+  isSigning = true;
+  try {
+    return library.signMessage(signMessage);
+  } finally {
+    isSigning = false;
+  }
+  // return library.signMessage(signMessage);
 };
