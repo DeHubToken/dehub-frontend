be3ea5b5894cb36faec7254c5e87e936c80f633a
Alhaji <alhaji4megahit@gmail.com>
2025-06-04 02:12:04 +0100

    fix: updated websockets

--- contexts/websocket.tsx
+++ contexts/websocket.tsx
@@ -6,6 +6,104 @@ import { useActiveWeb3React } from "@/hooks/web3-connect";
 import { env } from "@/configs";
 import { getAuthObject } from "@/web3/utils/web3-actions";
 
+class StreamingClient {
+  private socket: Socket | null = null;
+  private reconnectAttempts = 0;
+  private maxReconnectAttempts = 5;
+  private heartbeatInterval: NodeJS.Timeout | null = null;
+  private isMounted = true;
+  private onlineUsersCallback: ((users: string[]) => void) | null = null;
+
+  constructor(
+    private url: string, 
+    private getAuthParams: () => Promise<any>,
+    private onOnlineUsers: (users: string[]) => void
+  ) {
+    this.onlineUsersCallback = onOnlineUsers;
+  }
+
+  public async connect(address: string | null | undefined) {
+    if (this.socket) {
+      this.cleanup();
+    }
+
+    const socketOptions = {
+      query: { address },
+      reconnection: true,
+      reconnectionDelay: 1000,
+      reconnectionDelayMax: 5000,
+      reconnectionAttempts: this.maxReconnectAttempts,
+    };
+
+    const authObject = address ? await this.getAuthParams() : {};
+    
+    this.socket = io(this.url, {
+      auth: authObject,
+      ...socketOptions
+    });
+
+    this.setupEventHandlers();
+    this.startHeartbeat();
+  }
+
+  private setupEventHandlers() {
+    if (!this.socket) return;
+
+    this.socket.on('connect', () => {
+      console.log('Connected to streaming server');
+      this.reconnectAttempts = 0;
+    });
+
+    this.socket.on('disconnect', (reason) => {
+      console.log('Disconnected:', reason);
+      if (reason === 'io server disconnect') {
+        // Server initiated disconnect, try to reconnect
+        this.socket?.connect();
+      }
+    });
+
+    this.socket.on('connect_error', (error) => {
+      console.error('Connection error:', error);
+      this.reconnectAttempts++;
+      
+      if (this.reconnectAttempts >= this.maxReconnectAttempts) {
+        console.error('Max reconnection attempts reached');
+        this.cleanup();
+      }
+    });
+
+    this.socket.on("update-online-users", (users: string[]) => {
+      if (this.isMounted && this.onlineUsersCallback) {
+        this.onlineUsersCallback(users);
+      }
+    });
+  }
+
+  private startHeartbeat() {
+    this.heartbeatInterval = setInterval(() => {
+      if (this.socket?.connected) {
+        this.socket.emit('heartbeat');
+      }
+    }, 10000); // 10 seconds
+  }
+
+  public cleanup() {
+    this.isMounted = false;
+    if (this.heartbeatInterval) {
+      clearInterval(this.heartbeatInterval);
+      this.heartbeatInterval = null;
+    }
+    if (this.socket) {
+      this.socket.disconnect();
+      this.socket = null;
+    }
+  }
+
+  public getSocket() {
+    return this.socket;
+  }
+}
+
 const SocketsContext = createContext<any>({});
 
 export const useWebSockets = () => useContext(SocketsContext);
@@ -15,59 +113,37 @@ export const SERVER_URL = env.NEXT_PUBLIC_SOCKET_URL;
 export const WebsocketProvider = ({ children }: { children: React.ReactNode }) => {
   const [onlineUsers, setOnlineUsers] = useState<string[]>([]);
   const [socket, setSocket] = useState<Socket | null>(null);
-  const socketRef = useRef<Socket | null>(null);
+  const streamingClientRef = useRef<StreamingClient | null>(null);
   const { account, library } = useActiveWeb3React();
 
   useEffect(() => {
-    let isMounted = true;
-
-    const setUpSockets = async () => {
-      // If we already have a socket connection, don't create a new one
-      if (socketRef.current) {
-        socketRef.current.disconnect();
-      }
-
-      const socketOptions={
-        query: {
-          address: account
-        }, 
-      }
-
-      // TODO: update for stream chat
-      const authObject = account ? await getAuthObject(library, account as string) : {};
-      const socketIO = io(SERVER_URL, {
-        auth: authObject,
-        ...socketOptions
-      });
-
-      socketIO.on("update-online-users", (users: string[]) => {
-        if (isMounted) {
-          setOnlineUsers(users);
-        }
-      });
-
-      if (account) {
-        // socketIO.emit("join", account);
-        // socketIO.io.on("reconnect", () => {
-        //   socketIO.emit("join", account);
-        // });
-      }
-
-      if (isMounted) {
-        socketRef.current = socketIO;
-        setSocket(socketIO);
-      } else {
-        socketIO.disconnect();
-      }
+    const getAuth = async () => {
+      return await getAuthObject(library, account as string);
     };
 
-    setUpSockets();
+    if (!streamingClientRef.current) {
+      streamingClientRef.current = new StreamingClient(
+        SERVER_URL,
+        getAuth,
+        setOnlineUsers
+      );
+    }
+
+    streamingClientRef.current.connect(account);
+
+    // Update socket state when connection is established
+    const socketCheck = setInterval(() => {
+      const currentSocket = streamingClientRef.current?.getSocket();
+      if (currentSocket?.connected) {
+        setSocket(currentSocket);
+        clearInterval(socketCheck);
+      }
+    }, 100);
 
     return () => {
-      isMounted = false;
-      if (socketRef.current) {
-        socketRef.current.disconnect();
-        socketRef.current = null;
+      clearInterval(socketCheck);
+      if (streamingClientRef.current) {
+        streamingClientRef.current.cleanup();
       }
     };
   }, [account, library]);
